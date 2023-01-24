4f122479d22c4e546a6c456766556bdc09bb8b71
junhee <junhee0720@gmail.com>
2023-01-24 22:23:51 +0900

    과제 커밋
    첫 주 과제 생성 및 push

---
+++ src/pages/ComponentName.js
@@ -0,0 +1,9 @@
+const ComponentName = () => {
+    return (
+        <div style={{border: '1px solid red', padding : '15px'}}>
+            컴포넌트 페이지!!!
+        </div>
+    );
+}
+
+export default ComponentName;
